11ccab5f2593d3fbf37ce64df6c568a263499d4e
François Faucon <francois@MacBook-Pro-de-Francois-3.local>
2018-10-24 15:27:37 +0200

    cé changé

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,6 @@
 import hello from './components/hello';
 import { name } from './components/hello2';
+import addClass from './components/addClass';
 
 console.info(`Hello ${hello.name}`);
 console.info(`Hello ${name()}`);
